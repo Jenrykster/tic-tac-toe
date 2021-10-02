6ed3f1386b21520c6f954b5a7c3142a984baf6c5
João Henrique Santana <joaoenrique.san@gmail.com>
2021-10-02 00:41:51 -0300

    Add bruteforce logic to check gameover

--- js/index.js
+++ js/index.js
@@ -29,12 +29,31 @@ let gameManager = (function(){
 let gameBoard = (function(){
     let board = ["","","","","","","","",""]; // Array with empty strings to not mess order of elements
     const checkWinCond = () => {
-        if(board[0] == board[1] && board[1] == board[2] && board[1] != ""){
+        if(board[0] == board[1] && board[1] == board[2] && board[0]!=''){
             return true;
         }
-        else{
-            return false;
+        if(board[3] == board[4] && board[4] == board[5] && board[3]!=''){
+            return true;
         }
+        if(board[6] == board[7] && board[7] == board[8] && board[6]!=''){
+            return true;
+        }
+        if(board[0] == board[3] && board[3] == board[6] && board[0]!=''){
+            return true;
+        }
+        if(board[1] == board[4] && board[4] == board[7] && board[1]!=''){
+            return true;
+        }
+        if(board[2] == board[5] && board[5] == board[8] && board[2]!=''){
+            return true;
+        }
+        if(board[0] == board[4] && board[4] == board[8] && board[0]!=''){
+            return true;
+        }
+        if(board[2] == board[4] && board[4] == board[6] && board[2]!=''){
+            return true;
+        }
+        return false;
     }
     return {
         board,
